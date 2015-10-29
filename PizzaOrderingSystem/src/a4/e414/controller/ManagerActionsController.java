e70cd93f1829fb0596338cf4b53b0c93afb2976d
reddyanvesh <ks.anvesh@gmail.com>
2015-10-28 19:16:55 -0600

    Code for Manager actions

---
+++ PizzaOrderingSystem/src/a4/e414/controller/ManagerActionsController.java
@@ -0,0 +1,60 @@
+package a4.e414.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+
+import a4.e414.view.ManagerActionsScreen;
+
+public class ManagerActionsController 
+{
+	private ManagerActionsScreen homeScreenManager;
+	private String user;
+	
+	public ManagerActionsController(String user)
+	{
+		this.user=user;
+		this.homeScreenManager = new ManagerActionsScreen(user);
+		this.homeScreenManager.ButtonListener(new ManagerHomeActionListener());
+	}
+
+	class ManagerHomeActionListener implements ActionListener
+	{
+		@Override
+		public void actionPerformed(ActionEvent e) 
+		{
+			JButton tempButton = (JButton)e.getSource();
+		    			
+			if(tempButton.equals(homeScreenManager.getDefineMenuButton()))
+	        {
+				new CreateMenuController(user);
+				homeScreenManager.setVisible(false);
+	        }
+	        else if(tempButton.equals(homeScreenManager.getEditMenuButton())) 
+	        {
+	        	new ManageMenuController(user);
+	        	homeScreenManager.setVisible(false);
+	        }
+	        else if(tempButton.equals(homeScreenManager.getSetSpecialButton())) 
+	        {
+	        	homeScreenManager.setVisible(false);
+	        	new CreateSpecialController(user);
+	        }
+	        else if(tempButton.equals(homeScreenManager.getViewOrderButton())) 
+	        {
+	        	homeScreenManager.setVisible(false);
+	        	new DisplayOrderController(user);
+	        }
+/*	        else if(tempButton.equals(managerHomeView.getInventoryButton())) 
+	        {
+	        	managerHomeView.setVisible(false);
+	        	new InventoryController(user);
+	        }
+*/	        else if(tempButton.equals(homeScreenManager.getLogoutButton())) 
+	        {
+	        	homeScreenManager.setVisible(false);
+	        	new UserLoginController();
+	        }
+		}
+	}
+} 
