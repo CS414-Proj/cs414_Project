1c57f141d03afd3c0f023451550caf5530293e30
Sarish Arora <sarishar@makalu.cs.colostate.edu>
2015-10-28 20:02:40 -0600

    Cash Payment Model

---
+++ PizzaOrderingSystem/src/a4/e414/model/CashPaymentModel.java
@@ -0,0 +1,13 @@
+package a4.e414.model;
+
+public class CashPaymentModel {
+	
+	private float amountGiven;
+	private float change;
+	
+	public CashPaymentModel(float amountgiven, float change){
+		this.amountGiven = amountgiven;
+		this.change = change;
+	}
+	
+}
