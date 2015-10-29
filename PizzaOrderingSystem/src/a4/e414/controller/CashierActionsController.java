8eb521e134fcfda42b54956c907442a59172dc93
Sarish Arora <sarishar@makalu.cs.colostate.edu>
2015-10-28 20:12:40 -0600

    Cashier Action Controller

---
+++ PizzaOrderingSystem/src/a4/e414/controller/CashierActionsController.java
@@ -0,0 +1,44 @@
+package a4.e414.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import a4.e414.model.StoreOrderModel;
+import a4.e414.view.CashierActionsScreen;
+
+public class CashierActionsController {
+
+	private CashierActionsScreen cashierScreen;
+	private String user;
+
+	public CashierActionsController(String user){
+		this.user=user;
+		this.cashierScreen = new CashierActionsScreen(user);
+		this.cashierScreen.ButtonListner(new CashierHomeListner());
+	}
+
+	class CashierHomeListner implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			if(e.getSource()==cashierScreen.getTakeOrderButton()){
+				new StoreOrderModel(user,"cashier");
+				cashierScreen.setVisible(false);
+			}
+			else if(e.getSource()==cashierScreen.getLogOutButton()){
+				new UserLoginController();
+				cashierScreen.setVisible(false);
+			}
+			else if(e.getSource()==cashierScreen.getViewOrderbutton()){
+				new DisplayOrderController(user);
+				cashierScreen.setVisible(false);
+			}
+			else if(e.getSource()==cashierScreen.getViewSplButton()){
+				new DisplaySpecialController(user);
+				cashierScreen.setVisible(false);
+			}
+		}
+	}
+
+}
