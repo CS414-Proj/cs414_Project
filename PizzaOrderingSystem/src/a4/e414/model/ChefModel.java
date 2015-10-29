e4c84c3a7a032d4214e4193bf3737c12f11badb6
Sarish Arora <sarishar@makalu.cs.colostate.edu>
2015-10-28 20:03:27 -0600

    Chef Model

---
+++ PizzaOrderingSystem/src/a4/e414/model/ChefModel.java
@@ -0,0 +1,11 @@
+package a4.e414.model;
+
+import a4.e414.controller.ChefActionsController;
+
+public class ChefModel {
+
+	public ChefModel(String user){
+		new ChefActionsController(user);
+	}
+	
+}
