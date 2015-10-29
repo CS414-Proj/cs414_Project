ff9d4911965c25f9138de5ed385e61e285ffc110
reddyanvesh <ks.anvesh@gmail.com>
2015-10-28 20:34:20 -0600

    Logic/code for managing manager activities

---
+++ PizzaOrderingSystem/src/a4/e414/model/StoreManagerModel.java
@@ -0,0 +1,10 @@
+package a4.e414.model;
+
+import a4.e414.controller.ManagerActionsController;
+
+public class StoreManagerModel {
+
+	public StoreManagerModel(String user){
+		new ManagerActionsController(user);
+	}
+}
